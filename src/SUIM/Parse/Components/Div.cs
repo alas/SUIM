1a206ef34aea8ea74ff9d00c9bfb66122d90e3f4
Alas <mario.alastor@gmail.com>
2026-03-05 16:20:10 -0300

    div

--- src/SUIM/Parse/Components/Div.cs
+++ src/SUIM/Parse/Components/Div.cs
@@ -1,3 +1,17 @@
 namespace SUIM.Parse.Components;
 
-public class Div() : LayoutElement(nameof(Div)) { }
+using SUIM.Flexbox;
+
+/// <summary>
+/// A simple container where children are arranged in a vertical stack by default.
+/// Equivalent to a CSS block-level element (display: flex; flex-direction: column).
+/// </summary>
+public class Div : LayoutElement
+{
+    public Div() : base(nameof(Div))
+    {
+        Node.StyleSetDisplay(Display.Flex);
+        Node.StyleSetFlexDirection(FlexDirection.Column);
+        Node.StyleSetAlignItems(Align.Stretch);
+    }
+}
