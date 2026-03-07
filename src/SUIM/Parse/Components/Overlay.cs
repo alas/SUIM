b4c60227b78be4a529df42117811614e9589f640
Alas <mario.alastor@gmail.com>
2026-03-07 15:47:25 -0300

    width overlay

--- src/SUIM/Parse/Components/Overlay.cs
+++ src/SUIM/Parse/Components/Overlay.cs
@@ -17,6 +17,10 @@ public class Overlay : LayoutElement
         Node.StyleSetPosition(Edge.Right, 0);
         Node.StyleSetPosition(Edge.Top, 0);
         Node.StyleSetPosition(Edge.Bottom, 0);
+        Node.StyleSetWidthPercent(100);
+        Node.StyleSetHeightPercent(100);
         Node.StyleSetDisplay(Display.Flex);
+        Node.StyleSetJustifyContent(Justify.Center);
+        Node.StyleSetAlignItems(Align.Center);
     }
 }
